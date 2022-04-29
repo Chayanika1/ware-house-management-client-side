0bf9eef91cbbf415c2376b94d656edd414a14afe
chayanika chakraborty <2016chayanika@gmail.com>
2022-04-29 21:25:59 +0530

    email verified added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,23 +1,79 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSendPasswordResetEmail, useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { sendPasswordResetEmail } from 'firebase/auth';
+
+
+
+
+
+
+    
 
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+   
+    const handleEmail = (e) => {
+        setEmail(e.target.value)
+
+    }
+    const handlePassword = (e) => {
+        setPassword(e.target.value)
+
+    }
+    const handleLogin = (e) => {
+        signInWithEmailAndPassword(email, password)
+        e.preventDefault()
+    }
+    let errorElement;
+    if (user) {
+        navigate('/Home')
+    }
+    if (error) {
+        errorElement = <p>Error : {error.message}</p>
+    }
+    const resetPassword =  () => {
+        sendPasswordResetEmail(auth,email)
+        .then(()=>{
+            toast('Email send')
+        })
+
+        
+    }
     return (
         <div className='col-lg-6 col-sm-8 mx-auto'>
-            <form>
+            <form onSubmit={handleLogin}>
                 <div class="mb-3">
                     <label for="exampleInputEmail1" class="form-label">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required/>
+                    <input onBlur={handleEmail}type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required/>
                         <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
                 </div>
                 <div class="mb-3">
                     <label for="exampleInputPassword1" class="form-label">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" required/>
+                    <input onBlur={handlePassword} type="password" class="form-control" id="exampleInputPassword1" required/>
                 </div>
                 <div class="mb-3 form-check">
                     <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
                         <label class="form-check-label" for="exampleCheck1">Check me out</label>
                 </div>
+                {errorElement}
                 <button type="submit" class="btn btn-primary">Login</button>
+                <p>Don't have any account? then go to<Link className="p-3" style={{ textDecoration: 'none' }} to="/Register">Register</Link></p>
+                <p>Forget Password? <button className='btn btn-link text-primary pe-auto text-decoration-none' onClick={resetPassword}>Reset Password</button> </p>
+                <ToastContainer/>
             </form>
 
         </div>
--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,27 +1,90 @@
-import React from 'react';
+import React, { useState, useSyncExternalStore } from 'react';
+import { useCreateUserWithEmailAndPassword, useSendEmailVerification } from 'react-firebase-hooks/auth';
+import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
+import { ToastContainer, toast } from 'react-toastify';
+  import 'react-toastify/dist/ReactToastify.css';
+
+
+import { sendEmailVerification } from 'firebase/auth';
+
+
+
+    
 
 const Register = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [fixedError, setFixedError] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useCreateUserWithEmailAndPassword(auth);
+    //const [sendEmailVerification, sending] = useSendEmailVerification(
+    //    auth
+    //);
+    const navigate = useNavigate()
+
+    const handelEmail = (e) => {
+        setEmail(e.target.value)
+    }
+    const handelPassword = (e) => {
+        setPassword(e.target.value)
+    }
+    const handelConfirmPassword = (e) => {
+        setConfirmPassword(e.target.value)
+    }
+    const handleRegistration = (e) => {
+        if (password !== confirmPassword) {
+            alert('password not matched')
+        }
+
+        createUserWithEmailAndPassword(email, password, confirmPassword);
+        e.preventDefault();
+
+
+    }
+    let errorElement;
+    if (error) {
+        errorElement = <p>{error.message}</p>
+    }
+    if (user) {
+        navigate('/Home')
+    }
+    const verify=()=>{
+        sendEmailVerification(auth.currentUser)
+        .then(()=>{
+            toast('Email verification send')
+        })
+    }
     return (
         <div className='col-lg-6 col-sm-8 mx-auto'>
-             <form>
+             <form onSubmit={handleRegistration}>
                 <div class="mb-3">
                     <label for="exampleInputEmail1" class="form-label">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required/>
+                    <input onBlur={handelEmail} type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required/>
                         <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
                 </div>
                 <div class="mb-3">
                     <label for="exampleInputPassword1" class="form-label">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" required/>
+                    <input onBlur={handelPassword}type="password" class="form-control" id="exampleInputPassword1" required/>
                 </div>
                 <div class="mb-3">
                     <label for="exampleInputPassword1" class="form-label">Confirm Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" required/>
+                    <input onBlur={handelConfirmPassword} type="password" class="form-control" id="exampleInputPassword1" required/>
                 </div>
                 <div class="mb-3 form-check">
                     <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
                         <label class="form-check-label" for="exampleCheck1">Check me out</label>
                 </div>
-                <button type="submit" class="btn btn-primary">Register</button>
+                {errorElement}
+                
+                <button onClick={ verify }type="submit" class="btn btn-primary">Register</button>
+                <p>Already have an account? then go to<Link className="p-3" style={{ textDecoration: 'none' }} to="/Login">Login</Link></p>
+                <ToastContainer/>
             </form>
 
             
